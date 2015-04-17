810eef2f3d46a5d1cbab23ce9e2cc5f8f1b58a49
TinyTeaTree <iglizerin@gmail.com>
2015-04-17 20:26:39 +0300

    ship movement

---
+++ Assets/Scripts/SpaceShipController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+
+
+public class SpaceShipController : MonoBehaviour {
+
+    public float maxSpeed;
+    public float accelaration;
+    public float deaccelaration;
+
+    Vector2 currentVelocity = Vector2.zero;
+
+    Rigidbody myRigidbody;
+
+    void Awake() {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+
+        float deltaX = Input.GetAxis("Horizontal");
+        float deltaY = Input.GetAxis("Vertical");
+
+        Vector2 velocityVector = Vector2.zero;
+        velocityVector.x += deltaX;
+        velocityVector.y += deltaY;
+
+        velocityVector = velocityVector.normalized * Time.deltaTime * (accelaration + deaccelaration);
+
+        Vector2 deaccelarationVector = currentVelocity.normalized * -1 * deaccelaration * Time.deltaTime;
+
+        if (deaccelarationVector.magnitude > (currentVelocity + velocityVector).magnitude) {
+            currentVelocity = Vector2.zero;
+        } else {
+            currentVelocity = currentVelocity + velocityVector + deaccelarationVector;
+        }
+        
+        if (currentVelocity.magnitude > maxSpeed) {
+            currentVelocity = currentVelocity.normalized * maxSpeed;
+        }
+
+        Vector3 inverted;
+        inverted.x = currentVelocity.x;
+        inverted.y = 0;
+        inverted.z = currentVelocity.y;
+        //transform.Translate(inverted * Time.deltaTime);
+
+        myRigidbody.velocity = inverted;
+
+    }	
+
+}
