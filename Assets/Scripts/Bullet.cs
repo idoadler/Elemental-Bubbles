15a6a26991bee8bb9022b8156e4c32b54b52dd38
Ido Adler <redturt@gmail.com>
2015-04-18 02:35:15 +0300

    reflect

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.layer == LayerMask.NameToLayer("Boss"))
+		{
+			gameObject.layer = LayerMask.NameToLayer("Boss");
+			Destroy(gameObject.GetComponent<Rigidbody>());
+			gameObject.transform.SetParent(other.transform.parent);
+		}
+	}
+}
--- Assets/Scripts/WallCollision.cs
+++ Assets/Scripts/WallCollision.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class WallCollision : MonoBehaviour {
 
 	public Movement bossMovement;
+	public Vector3 velocityChange;
 
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.layer == LayerMask.NameToLayer("Boss"))
 			bossMovement.SetNextNodeOnPath();
 		else if (other.gameObject.layer == LayerMask.NameToLayer("Bubble"))
-			other.GetComponent<Rigidbody>().velocity = -other.GetComponent<Rigidbody>().velocity;
+			other.GetComponent<Rigidbody>().velocity = new Vector3 (other.GetComponent<Rigidbody>().velocity.x * velocityChange.x,other.GetComponent<Rigidbody>().velocity.y * velocityChange.y,other.GetComponent<Rigidbody>().velocity.z * velocityChange.z);
 	}
 }
